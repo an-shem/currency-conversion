e2cc60e19198001eb804fe3b56973783459e0038
Andrey Shemet <an-shem@ukr.net>
2025-06-20 01:51:00 +0200

    removed unnecessary annotation in the response object

--- src/main/java/ait/cohort5860/currencyconversion/dto/ConversionResponseForClientDto.java
+++ src/main/java/ait/cohort5860/currencyconversion/dto/ConversionResponseForClientDto.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class ConversionResponseForClientDto {
     public String from;
     public String to;
